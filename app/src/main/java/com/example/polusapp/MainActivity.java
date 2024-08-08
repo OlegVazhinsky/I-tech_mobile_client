d4ca6dd40ecb17fc0015a900a9f13334b7300221
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2024-08-09 00:25:04 +0700

    Connection procedure started. Validation methods in test.

--- app/src/main/java/com/example/polusapp/MainActivity.java
+++ app/src/main/java/com/example/polusapp/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import utils.TCPconnection;
 import utils.Validation;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,29 +23,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SimpleDateFormat time = new SimpleDateFormat("hh:mm:ss : ");
+
         TextView info = findViewById(R.id.textInfo);
+        TextView ipAddres = findViewById(R.id.editTextIP);
+        TextView port = findViewById(R.id.editTextPort);
+        TextView timeOut = findViewById(R.id.editTextTimeOut);
+
+        info.setMovementMethod(new ScrollingMovementMethod());
         Button buttonConnect = findViewById(R.id.buttonConnect);
         Button buttonDisconnect = findViewById(R.id.buttonDisconnect);
 
         Validation validate = new Validation();
 
-        info.append("\nСтарт программы.\n");
+        info.append("\n" + time.format(new Date()) + " - Старт программы.\n");
 
         WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         int ip = wm.getDhcpInfo().ipAddress;
 
         if (ip != 0){
-            info.append("IP адрес: " + validate.integerIPToString(ip) + ".\n");
+            info.append(time.format(new Date()) + " - IP адрес: " + validate.integerIPToString(ip) + ".\n");
         } else {
-            info.append("Не удалось подключиться к сети Wi-fi.\n");
+            info.append(time.format(new Date()) + " - Не удалось подключиться к сети Wi-fi.\n");
         }
 
         buttonConnect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (validate.isIP4validate(validate.integerIPToString(ip))) {
-                    info.append("Введенный IP адрес корректный. Пытаюсь подключиться к " + validate.integerIPToString(ip) + "...\n");
+                if (validate.isIP4validate(ipAddres.toString())) {
+                    info.append(time.format(new Date()) + " - Введенный IP адрес " + ipAddres.toString() + " корректный.\n");
+                    if (validate.isPortValidate(port.toString())) {
+                        info.append(time.format(new Date()) + " - Введенный порт " + port.toString() + " корректный.\n");
+                        info.append(time.format(new Date()) + " - Введенный порт (int) " + Integer.valueOf(port.toString()) + " корректный.\n");
+                        if (validate.isTimeoutValidate(timeOut.toString())) {
+                            info.append(time.format(new Date()) + " - Введенный таймаут " + timeOut.toString() + " корректный.\n");
+                        } else {
+                            info.append(time.format(new Date()) + " - Введен некорректный таймаут.\n");
+                        }
+                    } else {
+                        info.append(time.format(new Date()) + " - Введен некорректный порт.\n");
+                    }
                 } else {
-                    info.append("Введен некорректный IP адрес.\n");
+                    info.append(time.format(new Date()) + " - Введен некорректный IP адрес.\n");
                 }
             }
         });
